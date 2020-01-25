e5644793ea8a1ffe1dfd0e4696b2059f42af9f76
pirate-lofy <pirate.lofy@gmail.com>
2020-01-25 22:44:37 +0200

    step function

--- environment.py
+++ environment.py
@@ -1,6 +1,7 @@
 from carla.client import CarlaClient
 from carla.settings import CarlaSettings
-from carla.settings import Camera
+from carla.sensor import Camera
+from carla.tcp import TCPConnectionError
 from carla.image_converter import to_rgb_array,labels_to_cityscapes_palette
 
 import random
@@ -8,12 +9,16 @@ import cv2 as cv
 import numpy as np
 
 class CarlaEnv:
-    n_vehicles=0
-    n_peds=0
+    n_vehicles=60
+    n_peds=10
     n_actions=3
     
     observation_shape=(600,800,3)
-    callibaration_shape=(84,84,3)
+    callibaration_shape=(84,84)
+    
+    speed_list=[]
+    speed_list_limit=10
+    wait=15
     
     cameras={'rgb':'SceneFinal',
              'seg':'SemanticSegmentation',
@@ -29,11 +34,11 @@ class CarlaEnv:
     def start(self):
         self.client.connect()
         print('CarlaEnv log: client connected successfully.')
-        
         self.scene=self.client.load_settings(self.settings)
         
     def reset(self):
         self._start_new_episod()
+        self._empty_cycle()
         data,measures=self._get_data(reset=True)
         return data,measures
     
@@ -56,10 +61,36 @@ class CarlaEnv:
         done=self._is_done(measures)
         return (data,measures),reward,done,{}
     
+    
+    def _empty_cycle(self):
+        print('CarlaEnv log: empty cycle started...')
+        for _ in range(self.wait):
+            self.client.read_data()
+            self.client.send_control(
+                steer=0,
+                throttle=0,
+                brake=0,
+                hand_brake=False,
+                reverse=False                
+                )
+        print('CarlaEnv log: empty cycle ended.')
+    
     def _is_done(self,measures):
         dist=measures[1]
+        return self._is_goal(dist) or self._is_bad_pos(measures)
+                
+    
+    def _is_bad_pos(self,measures):
         colls=measures[2]
-        return self._is_goal(dist) and self._did_collide(colls)
+        inters=measures[3]
+        return self._did_collide(colls) or self._sidewalk(inters)\
+            or self._is_quiet()
+
+                
+    def _is_quiet(self):
+        if len(self.speed_list)>self.speed_list_limit:
+            self.speed_list=self.speed_list[1:self.speed_list_limit+1]
+        return sum(self.speed_list)==0
     
     #TODO: should be revised
     def _compute_reward(self,measures):
@@ -72,16 +103,18 @@ class CarlaEnv:
         
         if self._is_goal(dist):
             return 1000
-        if self._did_collide(colls):
+        if self._is_bad_pos(measures):
             return -1000
         r=alpha*(speed+dist-inters)
         return r
         
+    def _sidewalk(self,inters):
+        return inters>0.3
     
-    def _is_goal(dist):
+    def _is_goal(self,dist):
         return dist<2.0
     
-    def _did_collide(colls):
+    def _did_collide(self,colls):
         return colls>0
     
     def _get_data(self,reset=False):
@@ -91,14 +124,14 @@ class CarlaEnv:
             self.start_time=measures.game_timestamp
         
         data=self._data_preprocessing(data)
-        state,colls,inters=self._measures_preprocessing(measures) 
+        state,colls,inters=self._measures_preprocessing(measures)
         measures=self._make_measures_blob(state,colls,inters)
     
         return data,measures
     
     def _make_measures_blob(self,state,colls,inters):
         pos=np.array(state[0:2])
-        dist_to_goal=np.linal.norm(pos-self.goal)
+        dist_to_goal=np.linalg.norm(pos-self.goal)
         
         speed=state[2]
         colls=sum(colls)
@@ -111,8 +144,10 @@ class CarlaEnv:
 
         pos_x = PM.transform.location.x / 100 # cm -> m
         pos_y = PM.transform.location.y / 100
-        speed = PM.forward_speed/10.0 if PM.forward_speed>0 else 0.0
-
+        
+        speed = PM.forward_speed/10.0 if PM.forward_speed>=0 else 0.0001
+        self.speed_list.append(speed)
+        
         col_cars = PM.collision_vehicles
         col_ped = PM.collision_pedestrians
         col_other = PM.collision_other
@@ -125,14 +160,18 @@ class CarlaEnv:
     def _data_preprocessing(self,data):
         ready_data=[]
         for key in self.cameras.keys():
-            img=data[key].data
+            img,img_data=data[key],data[key].data
             
+            if key=='seg':
+                img_data=labels_to_cityscapes_palette(img)
             if not key=='depth':
-                img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+                img_data=cv.cvtColor(img_data.astype('float32'),cv.COLOR_BGR2GRAY)
             
-            img=np.resize(img,self.callibaration_shape)/255.0
-            ready_data.append(img)
+#            img_data=np.resize(img_data,self.callibaration_shape)/255.0
+            ready_data.append(img_data)
         
+        # TODO: check the stack technique to get 
+        # RGB-like tensor
         return np.stack(ready_data,axis=2)
         
     def _start_new_episod(self):
--- train.py
+++ train.py
@@ -1,3 +1,32 @@
 from environment import CarlaEnv
+from environment import TCPConnectionError
+import random
+import cv2 as cv
 
 env=CarlaEnv()
+env.start()
+env.reset()
+
+def train():
+    for _ in range(10000):
+        (observation,_), reward, done, _ = env.step([random.uniform(-1.0, 1.0)
+                                                    ,0.5,0])
+
+        if done:
+            env.reset()
+        
+
+def main():
+    while True:
+        try:
+            train()
+        except TCPConnectionError:
+#            print('waiting for the server....')
+            pass
+
+
+if __name__=='__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('cancelled by the user')
