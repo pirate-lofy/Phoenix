100db4a693aee0132894e930fcc21b1ef53e6a17
pirate-lofy <pirate.lofy@gmail.com>
2020-01-31 00:19:24 +0200

    added imapla_cnn instead of the nature_cnn

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 from stable_baselines.a2c.utils import conv, fc, conv_to_fc
 
 def nature_cnn(unscaled_images):
@@ -14,48 +15,17 @@ def nature_cnn(unscaled_images):
     return activ(fc(h3, 'fc1', nh=512, init_scale=np.sqrt(2)))
 
 
-def impala_cnn(unscaled_images, depths=[16, 32, 32], use_batch_norm=False, dropout=0.0):
-    """
-    Model used in the paper "IMPALA: Scalable Distributed Deep-RL with
-    Importance Weighted Actor-Learner Architectures" https://arxiv.org/abs/1802.01561
-    code from coinrun: https://github.com/openai/coinrun/blob/master/coinrun/policies.py
-    """
-    #use_batch_norm = Config.USE_BATCH_NORM == 1
+def impala_cnn(unscaled_images, depths=[16,32,32]):
+    layer_num = 0
 
-    dropout_layer_num = [0]
-    dropout_assign_ops = []
-
-    def dropout_layer(out):
-        #if Config.DROPOUT > 0:
-        if dropout > 0:
-            print("DROPOUT HAS NOT BEEN ADDED")
-            out_shape = out.get_shape().as_list()
-
-            var_name = 'mask_' + str(dropout_layer_num[0])
-            batch_seed_shape = out_shape[1:]
-            batch_seed = tf.get_variable(var_name, shape=batch_seed_shape,
-                                         initializer=tf.random_uniform_initializer(minval=0, maxval=1), trainable=False)
-            batch_seed_assign = tf.assign(batch_seed, tf.random_uniform(batch_seed_shape, minval=0, maxval=1))
-            dropout_assign_ops.append(batch_seed_assign)
-
-            curr_mask = tf.sign(tf.nn.relu(batch_seed[None, ...] - dropout))
-
-            curr_mask = curr_mask * (1.0 / (1.0 - dropout))
-
-            out = out * curr_mask
-
-        dropout_layer_num[0] += 1
-
-        return out
+    def get_layer_num_str():
+        nonlocal layer_num
+        num_str = str(layer_num)
+        layer_num += 1
+        return num_str
 
     def conv_layer(out, depth):
-        out = tf.layers.conv2d(out, depth, 3, padding='same')
-        out = dropout_layer(out)
-
-        if use_batch_norm:
-            out = tf.contrib.layers.batch_norm(out, center=True, scale=True, is_training=True)
-
-        return out
+        return tf.layers.conv2d(out, depth, 3, padding='same', name='layer_' + get_layer_num_str())
 
     def residual_block(inputs):
         depth = inputs.get_shape()[-1].value
@@ -74,15 +44,13 @@ def impala_cnn(unscaled_images, depths=[16, 32, 32], use_batch_norm=False, dropo
         out = residual_block(out)
         return out
 
-    #out = images #coinrun scales images outside this function call,
-    scaled_images = tf.cast(unscaled_images, tf.float32)
-    out = scaled_images
+    out = tf.cast(unscaled_images, tf.float32)
+
     for depth in depths:
         out = conv_sequence(out, depth)
 
     out = tf.layers.flatten(out)
     out = tf.nn.relu(out)
-    out = tf.layers.dense(out, 256, activation=tf.nn.relu)
+    out = tf.layers.dense(out, 256, activation=tf.nn.relu, name='layer_' + get_layer_num_str())
 
-    #return out, dropout_assign_ops
-    return out #not doing dropout
+    return out
