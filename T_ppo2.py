9573a95eb5fa9d043491bd763d2f413dbddff354
pirate-lofy <pirate.lofy@gmail.com>
2020-02-17 20:26:12 +0200

    solving the Broken Pipe problem

--- T_ppo2.py
+++ T_ppo2.py
@@ -3,6 +3,7 @@ from stable_baselines import logger
 import time
 import numpy as np
 import os
+import shutil
 
 
 def safemean(xs):
@@ -45,12 +46,12 @@ def learn(model,runner,n_epochs,n_steps,n_min_patches,n_opt_epochs,n_batch,
         
         lossvals = np.mean(mblossvals, axis=0)
         tnow = time.time()
-        fps = int(n_batch / (tnow - tstart))
+        fps = 1.0 / (tnow - tstart)
         
         # log to print results
         if update % log_interval == 0 or update == 1:
+            logger.logkv("epoch", update)
             logger.logkv("serial_timesteps", update*n_steps)
-            logger.logkv("nupdates", update)
             logger.logkv("total_timesteps", update*n_batch)
             logger.logkv("fps", fps)
             logger.logkv('returnmean', safemean(returns))
@@ -58,15 +59,21 @@ def learn(model,runner,n_epochs,n_steps,n_min_patches,n_opt_epochs,n_batch,
             for (lossval, lossname) in zip(lossvals, model.loss_names):
                 logger.logkv(lossname, lossval)
             logger.dumpkvs()
-        
+            
+#            model.log()
+            
         # save model
         if save_each and (update % save_each == 0 or update == 1):
             savepath=os.path.join(os.getcwd(),'checkpoints')
+            if update==1:
+                shutil.rmtree(savepath)
             if not os.path.exists(savepath):
                 os.mkdir(savepath)
             savepath = os.path.join(savepath, '%.5i'%update)
             print('Saving to', savepath)
             model.save(savepath)
-
+        
+        
+        runner.env.reset()
                 
     print('PPO2 log: Training has ended.')
