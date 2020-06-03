6b0dcb93a9e12bf7db9eb0448177c379a80bd615
pirate-lofy <pirate.lofy@gamil.com>
2020-06-03 19:28:11 +0200

    added droupout and batch norm to impala

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 def impala_cnn(unscaled_images, depths=[16,32,32]):
     layer_num = 0
@@ -39,3 +40,70 @@ def impala_cnn(unscaled_images, depths=[16,32,32]):
     out = tf.layers.dense(out, 256, activation=tf.nn.relu, name='layer_' + get_layer_num_str())
 
     return out
+
+def impala_gen(unscaled_images, depths=[16, 32, 32], use_batch_norm=True, dropout=0.3):
+    dropout_layer_num = [0]
+    dropout_assign_ops = []
+
+    def dropout_layer(out):
+        #if Config.DROPOUT > 0:
+        if dropout > 0:
+            print("DROPOUT HAS NOT BEEN ADDED")
+            out_shape = out.get_shape().as_list()
+            num_features = np.prod(out_shape[1:])
+
+            var_name = 'mask_' + str(dropout_layer_num[0])
+            batch_seed_shape = out_shape[1:]
+            batch_seed = tf.get_variable(var_name, shape=batch_seed_shape,
+                                         initializer=tf.random_uniform_initializer(minval=0, maxval=1), trainable=False)
+            batch_seed_assign = tf.assign(batch_seed, tf.random_uniform(batch_seed_shape, minval=0, maxval=1))
+            dropout_assign_ops.append(batch_seed_assign)
+
+            curr_mask = tf.sign(tf.nn.relu(batch_seed[None, ...] - dropout))
+
+            curr_mask = curr_mask * (1.0 / (1.0 - dropout))
+
+            out = out * curr_mask
+
+        dropout_layer_num[0] += 1
+
+        return out
+
+    def conv_layer(out, depth):
+        out = tf.layers.conv2d(out, depth, 3, padding='same')
+        out = dropout_layer(out)
+
+        if use_batch_norm:
+            out = tf.contrib.layers.batch_norm(out, center=True, scale=True, is_training=True)
+
+        return out
+
+    def residual_block(inputs):
+        depth = inputs.get_shape()[-1].value
+
+        out = tf.nn.relu(inputs)
+
+        out = conv_layer(out, depth)
+        out = tf.nn.relu(out)
+        out = conv_layer(out, depth)
+        return out + inputs
+
+    def conv_sequence(inputs, depth):
+        out = conv_layer(inputs, depth)
+        out = tf.layers.max_pooling2d(out, pool_size=3, strides=2, padding='same')
+        out = residual_block(out)
+        out = residual_block(out)
+        return out
+
+    #out = images #coinrun scales images outside this function call,
+    scaled_images = tf.cast(unscaled_images, tf.float32)
+    out = scaled_images
+    for depth in depths:
+        out = conv_sequence(out, depth)
+
+    out = tf.layers.flatten(out)
+    out = tf.nn.relu(out)
+    out = tf.layers.dense(out, 256, activation=tf.nn.relu)
+
+    #return out, dropout_assign_ops
+    return out #not doing dropout
--- policy.py
+++ policy.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
-from base_policies import ActorCriticPolicy
-from feature_extractor import impala_cnn
+from base_policies import ActorCriticPolicy,RecurrentActorCriticPolicy
+from feature_extractor import impala_gen
+from stable_baselines.common.tf_util import batch_to_seq, seq_to_batch
+from stable_baselines.common.tf_layers import conv, linear, conv_to_fc, lstm
 
 
 class CustomPolicy(ActorCriticPolicy):
@@ -14,7 +16,7 @@ class CustomPolicy(ActorCriticPolicy):
         with tf.variable_scope("model", reuse=reuse):
             activ = tf.nn.relu
 
-            extracted_features = impala_cnn(self.processed_obs)
+            extracted_features = impala_gen(self.processed_obs)
             extracted_features = tf.layers.flatten(extracted_features)
             
             mes=activ(tf.layers.dense(self.processed_mes,16,name='mes1'))
@@ -27,7 +29,7 @@ class CustomPolicy(ActorCriticPolicy):
             pi_latent=activ(tf.layers.dense(pi,128,name='pi_latent'))
 
             vf_latent=activ(tf.layers.dense(concat,64,name='vf_latent'))
-            value_fn=activ(tf.layers.dense(vf_latent,64,name='value_fn'))
+            value_fn=linear(vf_latent, 'vf', 1)
 
             self.proba_distribution, self._policy, self.q_value = \
                 self.pdtype.proba_distribution_from_latent(pi_latent, vf_latent, init_scale=0.01)
@@ -53,3 +55,66 @@ class CustomPolicy(ActorCriticPolicy):
     def value(self, obs, measures, hl_command, state=None, mask=None):
         return self.sess.run(self.value_flat, {self.obs_ph: obs, self.mes_ph:measures,
                                                     self.hl_ph:hl_command})
+
+    
+    
+
+class LstmPolicy(RecurrentActorCriticPolicy):
+    recurrent = True
+
+    def __init__(self, sess, ob_space, mes_space, hl_space,ac_space, n_env, n_steps, n_batch, n_lstm=256, 
+                 reuse=False, layers=None,activ=tf.nn.relu, layer_norm=True):
+        # state_shape = [n_lstm * 2] dim because of the cell and hidden states of the LSTM
+        super(LstmPolicy, self).__init__(sess, ob_space, mes_space, hl_space, ac_space, n_env, n_steps, n_batch,
+                                         state_shape=(2 * n_lstm, ), reuse=reuse)
+
+        with tf.variable_scope("model", reuse=reuse):
+            '''
+            image part, impala
+            '''
+            extracted_features = impala_cnn(self.processed_obs)
+            input_sequence = batch_to_seq(extracted_features, self.n_env, n_steps)
+            masks = batch_to_seq(self.dones_ph, self.n_env, n_steps)
+            
+            rnn_output, self.snew = lstm(input_sequence, masks, self.states_ph, 'lstm1', n_hidden=n_lstm,
+                                         layer_norm=layer_norm)
+            rnn_output = seq_to_batch(rnn_output)
+            
+            '''
+            remaining part
+            '''
+            mes=activ(tf.layers.dense(self.processed_mes,16,name='mes1'))
+            mes=activ(tf.layers.dense(mes,16,name='mes2'))
+            
+            concat=tf.concat([rnn_output,mes,self.processed_hl],axis=1,name='concat1')
+            concat=activ(tf.layers.dense(concat,128,name='concat2'))
+            
+            pi=activ(tf.layers.dense(concat,128,name='pi'))
+            pi_latent=activ(tf.layers.dense(pi,128,name='pi_latent'))
+
+            vf_latent=activ(tf.layers.dense(concat,64,name='vf_latent'))
+            value_fn=linear(vf_latent, 'vf', 1)
+#
+            self.proba_distribution, self._policy, self.q_value = \
+                self.pdtype.proba_distribution_from_latent(pi_latent, vf_latent, init_scale=0.01)
+            print('%%%%%%%%^^^^^^^&^&&&&&&&&&',type(value_fn[:, 0]))
+            self.value_fn = value_fn
+        self._setup_init()
+
+    def step(self, obs, measures, hl_command, state=None, mask=None, deterministic=False):
+        if deterministic:
+            return self.sess.run([self.deterministic_action, self.value_flat, self.snew, self.neglogp],
+                                 {self.obs_ph: obs, self.mes_ph:measures,self.hl_ph:hl_command, 
+                                  self.states_ph: state, self.dones_ph: mask})
+        else:
+            return self.sess.run([self.action, self.value_flat, self.snew, self.neglogp],
+                                 {self.obs_ph: obs, self.mes_ph:measures,self.hl_ph:hl_command, 
+                                  self.states_ph: state, self.dones_ph: mask})
+
+    def proba_step(self, obs, measures, hl_command, state=None, mask=None):
+        return self.sess.run(self.policy_proba, {self.obs_ph: obs, self.mes_ph:measures,self.hl_ph:hl_command, 
+                                                 self.states_ph: state, self.dones_ph: mask})
+
+    def value(self, obs, measures, hl_command, state=None, mask=None):
+        return self.sess.run(self.value_flat, {self.obs_ph: obs, self.mes_ph:measures,self.hl_ph:hl_command, 
+                                               self.states_ph: state, self.dones_ph: mask})
