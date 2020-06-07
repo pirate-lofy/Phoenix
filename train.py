076e88bc5a6c0edbced91b7734dce21b06fe8bfa
pirate-lofy <pirate.lofy@gamil.com>
2020-06-07 22:45:46 +0200

    specidied 16 envs

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ try:
         set_global_seeds(seed)
         return _init
     
-    env = DummyVecEnv([make_env(i) for i in range(100)])
+    env = DummyVecEnv([make_env(i) for i in range(16)])
     model=PPO2(CustomPolicy,env,save_each=10)
     model.learn(100000)
 except Exception:
